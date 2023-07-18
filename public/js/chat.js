c157cfaaa50d1567b89efeae5230c3ca99e34d3e
gautamraj873 <gautamraj873@gmail.com>
2023-07-18 23:04:26 +0530

    retrieve new msg every 1 sec using setInterval

--- public/js/chat.js
+++ public/js/chat.js
@@ -103,5 +103,9 @@ async function getMessages() {
   }
 }
 
+setInterval(() => {
+  getMessages();
+}, 1000);
+
 messageSendBtn.addEventListener("click", messageSend);
 document.addEventListener("DOMContentLoaded", getMessages);
